5eacc94a2efc21718621934cc51993edccfd0dc8
AmishaNair21 <amishamnair2002@gmail.com>
2024-09-13 13:25:03 +0530

    schema updated to images

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Create T3 App
 
 ## Todo 
-- [ ] Make it deploy
-- [ ] Scaffold basic ui with mock data
-- [ ] Actually set up a database(vercel postgres)
-- [ ] Attach database to ui
-- [ ] Add authentication (w/ clerk)
+- [x] Make it deploy
+- [x] Scaffold basic ui with mock data
+- [x] Actually set up a database(vercel postgres)
+- [x] Attach database to ui
+- [] Add authentication (w/ clerk)
 - [ ] Add image upload 
 - [ ] Error Management (w/ sentry)
 - [ ] Routing/image page(parallel route)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,21 @@
-
+import { images } from "~/server/db/schema";
 import { db } from "~/server/db";
 
 
 export const dynamic ="force-dynamic";
 
-const mockUrls =[
-  "https://utfs.io/f/4ad89fbc-8bec-4f7c-8fef-8564ecc761ff-twuc9d.png",
-  "https://utfs.io/f/cfd8ff2e-e664-41ee-b92d-7611116b41fe-c7xf8f.png",
- ' https://utfs.io/f/62d70517-39b9-4b64-93d5-4055714067ba-oxilxv.png',
- "https://utfs.io/f/8dbd1401-bf2b-4ecd-a8e8-56f3093e420d-13prjo.png"
-]
-
-const mockImages = mockUrls.map((url,index)=>({
-  id: index + 1,
-  url,
-}));
 
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const fetchedImages = await db.select().from(images);
   
   return (
     <main>
       <div className="flex flex-wrap gap-5">
-        {posts.map((post)=>(
-          <div key={post.id}>{post.name}</div>
-        ))}
+     
         {
-          [...mockImages,...mockImages,...mockImages].map((image,index)=>(
+          [...fetchedImages,...fetchedImages,...fetchedImages].map((image,index)=>(
             <div key={image.id + "-" + index} className="w-48">
               <img src={image.url}/>
             </div>
