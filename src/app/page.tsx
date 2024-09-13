bedc94f91d8af87bc50d6a577d5d5ffe4854bb05
AmishaNair21 <amishamnair2002@gmail.com>
2024-09-13 12:54:49 +0530

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
+
 import { db } from "~/server/db";
+
+
+export const dynamic ="force-dynamic";
 
 const mockUrls =[
   "https://utfs.io/f/4ad89fbc-8bec-4f7c-8fef-8564ecc761ff-twuc9d.png",
