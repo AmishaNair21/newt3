d1b651eb75dc4f38f1395aa54949a58ea1e4c2cb
AmishaNair21 <amishamnair2002@gmail.com>
2024-09-13 14:02:23 +0530

    auth by clerkdone

---
+++ src/app/_component/topnav.tsx
@@ -0,0 +1,17 @@
+import { SignedOut,SignInButton,SignedIn,UserButton } from "@clerk/nextjs";
+
+ export default function TopNav(){
+    return(
+      <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+          <div>Gallery</div>
+        <div>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    )
+  }
